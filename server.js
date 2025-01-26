2e11b2898327b63880da1de1cbecf49739ddeaf9
BeeBarry <barry.namdari@gmail.com>
2025-01-26 10:10:22 +0100

    endpoints added

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const mongoose = require ("mongoose");
 const PORT = 3000;
+const Product = require("./models/productModel");
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome to my mongoAPI")
@@ -14,6 +16,49 @@ app.post("/blogpost", (req, res) => {
 });
 
 
+app.put("/api/product/:id", async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id,req.body);
+    
+    if(!product){
+        return res.status(400).json({message: "product does not exist"});
+    }
+    
+    res.status(200).json(product);
+})
+
+
+app.get("/api/product/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch(error) {
+        res.status(500);
+    }
+})
+
+app.get("/api/products", async (req,res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        
+    }
+});
+
+app.post
+("/api/product", async(req, res) => {
+    
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(404);
+    }
+});
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("connected to mongo");
